2bbbd0572c7045df0db208580ab9f84f67cfb75e
Mohd-Sami010 <156431982+Mohd-Sami010@users.noreply.github.com>
2026-05-30 12:38:27 +0530

    Fixed that Crazy Games Preview issue

---
+++ Assets/Scripts/ErrorDetectorUI.cs
@@ -0,0 +1,58 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ErrorDetectorUI : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI errorTextMesh;
+    [SerializeField] private Button closeButton;
+    // [SerializeField] private Button copyButton;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        closeButton.onClick.AddListener(() =>
+        {
+            Hide();
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        });
+        // copyButton.onClick.AddListener(() => {
+        //     try
+        //     {
+        //         GUIUtility.systemCopyBuffer = errorTextMesh.text;
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         Debug.LogException(new Exception("Failed to Copy to clipboard!\n" + e));
+        //     }
+        // });
+    }
+    private void Start()
+    {
+        Application.logMessageReceived += Application_logMessageReceived;
+        Hide();
+    }
+
+    private void Application_logMessageReceived(string condition, string stackTrace, LogType type)
+    {
+        if (type == LogType.Error || type == LogType.Exception)
+        {
+            Show();
+
+            errorTextMesh.text = "Error: " + condition + "\n" + stackTrace;
+        }
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+    }
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
