1bb1d93d81220d10ec2145f3097a08677d91248d
Mohd-Sami010 <156431982+Mohd-Sami010@users.noreply.github.com>
2026-06-07 23:01:54 +0530

    Organized scripts in folders and added an AdUI

---
+++ Assets/Scripts/UI/AdUI.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AdUI : MonoBehaviour
+{
+    [SerializeField] private GameObject adLoadingUI;
+    [SerializeField] private GameObject adFailedUI;
+
+    private void Awake()
+    {
+        adLoadingUI.SetActive(false);
+        adFailedUI.SetActive(false);
+    }
+    public void ShowAdLoadingUI()
+    {
+        adLoadingUI.SetActive(true);
+        adFailedUI.SetActive(false);
+    }
+    public void ShowAdFailedUI()
+    {
+        adLoadingUI.SetActive(false);
+        adFailedUI.SetActive(true);
+    }
+}
